2bb321126de4045b63936c244d5d40eaad5ab64d
NiharikaMN81 <niharikamn404@gmail.com>
2026-07-18 18:06:20 +0530

    Completed JWT handson given in Spring REST using Spring Boot 3

---
+++ jwt-handson/src/main/java/com/cognizant/jwt_handson/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.cognizant.jwt_handson.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/authenticate").hasAnyRole("USER", "ADMIN")
+                .anyRequest().authenticated()
+            )
+            .httpBasic(withDefaults());
+
+        return http.build();
+    }
+        @Bean
+    UserDetailsService users() {
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("pwd")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
